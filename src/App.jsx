283b4292341bb2481bed2afdeb38090a2cd0a8c8
OLGA BRAIKO <Olga.braiko456@mail.ru>
2023-08-14 20:13:01 -0700

    useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
+import React, { useState } from "react";
+
 import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const OnclickPlus = () => {
+    setCount(count + 1);
+  };
+
+  const OnclickMinus = () => {
+    setCount(count - 1);
+  };
+
   return (
     <div className="App">
       <div>
         <h2>Счётчик</h2>
-        <h1>0</h1>
-        <button className="minus">- Минус</button>
-        <button className="plus">Плюс +</button>
+        <h1>{count}</h1>
+        <button onClick={OnclickMinus} className="minus">
+          - Минус
+        </button>
+        <button onClick={OnclickPlus} className="plus">
+          Плюс +
+        </button>
       </div>
     </div>
   );
